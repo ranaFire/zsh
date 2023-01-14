88ce401930322d37bb82ab8ecf745f8594d71830
ranafire <32353518+ranaFire@users.noreply.github.com>
2023-01-14 17:14:42 +0200

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -7,12 +7,12 @@ echo " ${RED}GIT INSTALL...................${NC}"
 apt install git -y
 echo "${RED}zsh INSTALL...................${NC}"
 apt install zsh -y
-echo "${RED}Adding autosuggestions...................${NC}"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 echo "${RED}ACTIVE ZSH...................${NC}"
 zsh
 echo "${RED}ohmyzsh  install...................${NC}"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "${RED}Adding autosuggestions...................${NC}"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 echo "${RED}replacing  .zshrc settings...................${NC}"
 curl -o ~/.zshrc https://raw.githubusercontent.com/ranaFire/zsh/main/.zshrc
 echo "${RED}refresing...................${NC}"
