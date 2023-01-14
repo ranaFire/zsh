6b0418ddf91d53247f5f840f7f4212c81eb065fd
ranafire <32353518+ranaFire@users.noreply.github.com>
2023-01-14 17:24:20 +0200

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -21,4 +21,8 @@ source ~/.zshrc
 zsh
 echo "${RED}DONE...................${NC}"
 
+# README
+# docker run -v $(pwd):/work -it ubuntu bash
+# apt update
+# apt install curl
 #curl https://raw.githubusercontent.com/ranaFire/zsh/main/zsh.sh | bash
