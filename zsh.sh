419f4fc521b521a9779d42b4e4adff86853fb8fe
ranafire <32353518+ranaFire@users.noreply.github.com>
2023-01-14 17:11:32 +0200

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-echo "GIT INSTALL..................."
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+
+echo " ${RED}GIT INSTALL...................${NC}"
 apt install git -y
-echo "zsh INSTALL..................."
+echo "${RED}zsh INSTALL...................${NC}"
 apt install zsh -y
-echo "ACTIVE ZSH..................."
+echo "${RED}Adding autosuggestions...................${NC}"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+echo "${RED}ACTIVE ZSH...................${NC}"
 zsh
-echo "ohmyzsh  install..................."
+echo "${RED}ohmyzsh  install...................${NC}"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "replacing  .zshrc settings..................."
+echo "${RED}replacing  .zshrc settings...................${NC}"
 curl -o ~/.zshrc https://raw.githubusercontent.com/ranaFire/zsh/main/.zshrc
-echo "refresing..................."
+echo "${RED}refresing...................${NC}"
 source ~/.zshrc
-echo "DONE..................."
+zsh
+echo "${RED}DONE...................${NC}"
 
 #curl https://raw.githubusercontent.com/ranaFire/zsh/main/zsh.sh | bash
