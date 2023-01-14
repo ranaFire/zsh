357a8ae34aee391a010a9fcd14086e88841d2981
ranafire <32353518+ranaFire@users.noreply.github.com>
2023-01-14 16:19:42 +0200

    Create zsh.sh

---
+++ zsh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get update
+apt-get install zsh
+apt-get install curl
+apt-get install git
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -o ~/.zshrc https://raw.githubusercontent.com/ranaFire/zsh/main/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+source ~/.zshrc
