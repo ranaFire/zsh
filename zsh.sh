b89f3205ae433c6b3e0b2e678fd7e7b5f2b79fc1
ranafire <32353518+ranaFire@users.noreply.github.com>
2023-01-14 16:22:21 +0200

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -8,3 +8,5 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 curl -o ~/.zshrc https://raw.githubusercontent.com/ranaFire/zsh/main/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 source ~/.zshrc
+
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ranaFire/zsh/main/zsh.sh)"
