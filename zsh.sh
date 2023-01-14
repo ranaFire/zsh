2891b1617f61f448830bab04bb6fee5bd3c09773
ranafire <32353518+ranaFire@users.noreply.github.com>
2023-01-14 16:52:22 +0200

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt install zsh -y
 zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -y)" -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -o ~/.zshrc https://raw.githubusercontent.com/ranaFire/zsh/main/.zshrc
 source ~/.zshrc
 
