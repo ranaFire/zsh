92b1baf0808856c541989691a4fb5bc0d57bcfa4
ranafire <32353518+ranaFire@users.noreply.github.com>
2023-01-14 16:34:22 +0200

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -6,7 +6,6 @@ apt-get install curl -y
 apt-get install git -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -o ~/.zshrc https://raw.githubusercontent.com/ranaFire/zsh/main/.zshrc
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 source ~/.zshrc
 
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ranaFire/zsh/main/zsh.sh)"
+#curl https://raw.githubusercontent.com/ranaFire/zsh/main/zsh.sh | bash
